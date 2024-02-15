51600364959ade4c03bc7f4abb42c38ce4840114
ValeriaRabch <rabchenukv@gmail.com>
2024-02-15 14:55:31 +0200

    Add func PrintWachingMachine

--- FileName.cpp
+++ FileName.cpp
@@ -2,6 +2,20 @@
 #include <string.h>
 using namespace std;
 
+struct WashingMachine  //1
+{
+	char firma[20], color[10];	
+	int width, height, power, temperature;
+};
+
+void PrintWashingMachine(WashingMachine machine) {  //1	
+	cout << "Firma -" << machine.firma << endl;
+	cout << "Color -" << machine.color << endl;	
+	cout << "Width -" << machine.width << endl;
+	cout << "Height -" << machine.height << endl;	
+	cout << "Power -" << machine.power << endl;
+	cout << "Temperature -" << machine.temperature << endl;
+}
 
 int main() 
 {
