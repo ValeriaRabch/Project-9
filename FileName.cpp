06953d25037f45bfb7129051231d276e92a661c9
ValeriaRabch <rabchenukv@gmail.com>
2024-02-15 14:59:02 +0200

    Add func PrintIron

--- FileName.cpp
+++ FileName.cpp
@@ -8,6 +8,13 @@ struct WashingMachine  //1
 	int width, height, power, temperature;
 };
 
+struct Iron     //2
+{
+	char firma[20], model[20], color[10];
+	int min_temperature, max_temperature, power;
+	bool steam; 
+};
+
 void PrintWashingMachine(WashingMachine machine) {  //1	
 	cout << "Firma -" << machine.firma << endl;
 	cout << "Color -" << machine.color << endl;	
@@ -17,13 +24,24 @@ void PrintWashingMachine(WashingMachine machine) {  //1
 	cout << "Temperature -" << machine.temperature << endl;
 }
 
+void PrintIron(Iron iron) {  //2	
+	cout << "Firma -" << iron.firma << endl;
+	cout << "Color -" << iron.color << endl;	
+	cout << "Model -" << iron.model << endl;
+	cout << "Min Temperature -" << iron.min_temperature << endl;	
+	cout << "Max Temperature -" << iron.max_temperature << endl;
+	cout << "Steam -" << iron.steam << endl;
+}
+
 int main() 
 {
 	//завдання 1
-	WashingMachine machine;
+	/*WashingMachine machine;
 	cout << "Enter \n 1.Firma \n2.Color\n3.Widht\n4.Height\n5.Power\n6.Temperature" << endl;
 	cin >> machine.firma >> machine.color;
 	cin >> machine.width >> machine.height >> machine.power >> machine.temperature;
-	PrintWashingMachine(machine);
+	PrintWashingMachine(machine);*/
+
+
 	return 0;
 }
